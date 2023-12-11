9466f24b6b7d99700207962103932c04403beb66
lafree317 <lafree317@163.com>
2023-12-11 19:26:11 +0800

    update

---
+++ FRG/Assets/Scripts/GaneStatus.cs
@@ -0,0 +1,7 @@
+public enum GameStatusEnum
+{
+    Ready,
+    Playing,
+    Pause,
+    End,
+}
--- FRG/Assets/Scripts/Main.cs
+++ FRG/Assets/Scripts/Main.cs
@@ -7,12 +7,21 @@ public class Main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void Test()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            GameObject go = new GameObject();
+            
+        }
     }
 }
